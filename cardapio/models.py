f1c2d7f0db208572ca937f5effc46609458cb915
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-02 17:56:38 -0300

    commit 3

--- cardapio/models.py
+++ cardapio/models.py
@@ -19,3 +19,11 @@ class Produto(models.Model):
 
     def __str__(self):
         return "{} - R$ {}".format(self.nome, self.preco)
+
+
+class Pedidos(models.Model):
+    cliente = models.CharField(max_length=200, blank=False)
+    endereco = models.CharField(max_length=200, blank=False)
+    descricao = models.TextField(blank=False)
+    lista_produtos = models.JSONField(blank=False)
+    preco_total = models.DecimalField(max_length=20, blank=False)
